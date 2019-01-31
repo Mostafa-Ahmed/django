8d53345022dda4b77c99d1b6154da15c537d3e4f
Mostafa-Ahmed <mostafa0100334@gmail.com>
2019-01-31 20:18:42 +0200

    favorite form established

--- music/views.py
+++ music/views.py
@@ -1,5 +1,26 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
-    return HttpResponse('<H1>this tha Music app homepage <H1>')
+    all_Albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_Albums': all_Albums})
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/favorite.html', {
+            'album': album,
+            'error_message': 'You didn\'t select a valid song '
+        })
+    selected_song.is_favorite = True
+    selected_song.save()
+    return render(request, 'music/detail.html', {'album': album})
